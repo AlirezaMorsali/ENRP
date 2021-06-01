ee2baef2c7c9e9d1fd0102010b10c534e6c76b19
hoenza <h.entezari78@ut.ac.ir>
2021-06-01 14:42:28 +0000

    clean

---
+++ codes/Siren.py
@@ -0,0 +1,59 @@
+class Siren(nn.Module):
+    def __init__(self, in_features, hidden_features, hidden_layers, out_features, outermost_linear=False, 
+                 first_omega_0=30, hidden_omega_0=30.0, new=False):
+        super().__init__()
+        
+        self.net = []
+        self.net.append(SineLayer(in_features, hidden_features, 
+                                  is_first=True, omega_0=first_omega_0, new=new))
+
+        for i in range(hidden_layers):
+            self.net.append(SineLayer(hidden_features, hidden_features, 
+                                      is_first=False, omega_0=hidden_omega_0, new=new))
+
+        if outermost_linear:
+            final_linear = nn.Linear(hidden_features, out_features)
+            
+            with torch.no_grad():
+                final_linear.weight.uniform_(-np.sqrt(6 / hidden_features) / hidden_omega_0, 
+                                              np.sqrt(6 / hidden_features) / hidden_omega_0)
+                
+            self.net.append(final_linear)
+        else:
+            self.net.append(SineLayer(hidden_features, out_features, 
+                                      is_first=False, omega_0=hidden_omega_0, new=new))
+        
+        self.net = nn.Sequential(*self.net)
+    
+    def forward(self, coords):
+        output = self.net(coords)
+        return output, None    
+
+    def forward_with_activations(self, coords, retain_grad=False):
+        '''Returns not only model output, but also intermediate activations.
+        Only used for visualizing activations later!'''
+        activations = OrderedDict()
+
+        activation_count = 0
+        x = coords.clone().detach().requires_grad_(True)
+        activations['input'] = x
+        for i, layer in enumerate(self.net):
+            if isinstance(layer, SineLayer):
+                x, intermed = layer.forward_with_intermediate(x)
+                
+                if retain_grad:
+                    x.retain_grad()
+                    intermed.retain_grad()
+                    
+                activations['_'.join((str(layer.__class__), "%d" % activation_count))] = intermed
+                activation_count += 1
+            else: 
+                x = layer(x)
+                
+                if retain_grad:
+                    x.retain_grad()
+                    
+            activations['_'.join((str(layer.__class__), "%d" % activation_count))] = x
+            activation_count += 1
+
+        return activations
